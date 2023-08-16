c43c9087f8b03f5726edd7b5841e74f5d54736b4
Jessica Odonchimeg <jessicaodonchimeg5@gmail.com>
2023-08-15 22:04:30 -0700

    finished product

--- ToDoList/ContentView.swift
+++ ToDoList/ContentView.swift
@@ -9,7 +9,12 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State var toDoItems: [ToDoItem] = []
+    @Environment(\.managedObjectContext) var context
+    
+    @FetchRequest(
+            entity: ToDo.entity(), sortDescriptors: [ NSSortDescriptor(keyPath: \ToDo.id, ascending: false) ])
+        
+    var toDoItems: FetchedResults<ToDo>
     @State private var showNewTask = false
     
     var body: some View {
@@ -30,25 +35,45 @@ struct ContentView: View {
                 
             }
             .padding()
-            
+         
+            Image("study")
+                .resizable(resizingMode: .stretch)
+                .aspectRatio(contentMode: .fit)
+                
         }
         .padding()
         
         List {
             ForEach (toDoItems) { toDoItem in
                 if toDoItem.isImportant == true {
-                    Text("‼️" + toDoItem.title)
+                    Text("‼️" + (toDoItem.title ?? "No title"))
                 } else {
-                    Text(toDoItem.title)
+                    Text(toDoItem.title ?? "No title")
                 }
             }
+            
+            .onDelete(perform: deleteTask)
+            
             }
         .listStyle(.plain)
             
-            if showNewTask {
-                NewToDoView(title: "", isImportant: false, toDoItems: $toDoItems, showNewTask: $showNewTask)
+        if showNewTask {
+            NewToDoView(title: "", isImportant: false, showNewTask: $showNewTask)
+        }
+        }
+    
+    private func deleteTask(offsets: IndexSet) {
+            withAnimation {
+                offsets.map { toDoItems[$0] }.forEach(context.delete)
+
+                do {
+                    try context.save()
+                } catch {
+                    print(error)
+                }
             }
         }
+    
     }
     
     struct ContentView_Previews: PreviewProvider {
